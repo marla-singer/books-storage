9810ac9cc2ffa15f887f1ed29195d559d3e58543
Daria Voytova <vdariaclem@gmail.com>
2017-01-14 20:04:17 +0300

    Read from input field

--- client/search-field/search-field.js
+++ client/search-field/search-field.js
@@ -19,12 +19,17 @@ Template.searchField.helpers({
 });
 
 Template.searchField.events({
-    'click #find-by-isbn'(event, instance) {
-        const isbn = "9785990808355";
+    'blur input' (event, instance) {
+      instance.isbn = event.currentTarget.value;
+    },
+    'submit #find-by-isbn-form'(event, instance) {
+        event.preventDefault();
+
+        // const isbn = "9785990808355";
         // const isbn = "9785995";
 
         // Find book by entering isbn
-        Meteor.call('findByISBN', isbn, (error, result) => {
+        Meteor.call('findByISBN', instance.isbn, (error, result) => {
             instance.information.set(result);
         });
     },
