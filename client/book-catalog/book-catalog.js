576cb3bc9a6c7d344e4f1755bc795e0e8962cbab
Daria Voytova <vdariaclem@gmail.com>
2017-01-22 16:20:27 +0300

    wip

---
+++ client/book-catalog/book-catalog.js
@@ -0,0 +1,33 @@
+import Books from '/collection/schema.js';
+
+Template.bookCatalog.onCreated(function () {
+   this.subscribe('allBooks');
+
+   // Initial reactive variable
+   this.selectedBook = new ReactiveVar();
+});
+
+Template.bookCatalog.helpers({
+    list () {
+        return Books.find().fetch();
+    },
+    bookDetails () {
+        const selectedBook = Template.instance().selectedBook.get();
+
+        // If book was selected then return book details otherwise doesn't show template
+        return selectedBook ? selectedBook.details : undefined;
+    }
+});
+
+Template.bookCatalog.events({
+    'click tr': (event, templateInstance) => {
+        // Get ID of selected book form data-id attribute
+        const bookId = event.currentTarget.dataset.id;
+        // Get related document from collection by ID
+        const book = Books.findOne(bookId);
+
+        // Save selected book in template instance
+        templateInstance.selectedBook.set(book);
+    },
+});
+
