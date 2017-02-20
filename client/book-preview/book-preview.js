7a3fc55019793972c47358932ccf2b3e3d3b0033
Daria Voytova <d.voytova@2glab.com>
2017-02-20 12:35:08 +0300

    Remove logic for select Date,rename helper, add updating for current document

--- client/book-preview/book-preview.js
+++ client/book-preview/book-preview.js
@@ -1,50 +1,19 @@
+// TODO: подключить библеотку momentjs
 import { Template } from 'meteor/templating';
-import range from 'lodash.range';
+
 import Books from '/collection/schema';
 
 Template.bookPreview.helpers({
-  days () {
-    // Auto generate days data with step 1
-    return range(1, 32, 1);
-  },
-  months () {
-    const standardMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-
-    const months = standardMonths.map((currentValue, index) => {
-      return {
-        label: currentValue,
-        value: index,
-        selected: null,
-      };
-    });
-
-    const currentMonth = new Date().getMonth();
-
-    // Set current month as selected by default
-    months[currentMonth].selected = 'selected';
-
-    return months;
-  },
-  years () {
-    const currentYear = new Date().getFullYear();
-    // Auto generate year data 2012-2030 with step 1
-    const yearsList = range(2012, currentYear, 1);
-    const years = yearsList.map((year) => {
-      return {
-        value: year,
-        selected: '',
-      };
-    });
-
-    // Set current year as selected by default
-    years.push({ value: currentYear, selected: 'selected' });
-
-    return years;
-  },
   ratingSet () {
     // Auto generate rating from 0 to 10 with step 1
     return range(1, 11, 1);
   },
+  readerList () {
+    // TODO: Реактивно обновлять список о читателях, когда добавили новую информацию
+    const book = Template.instance().data.book;
+
+    return book ? book.read_information : [];
+  }
 });
 
 Template.bookPreview.events({
@@ -52,10 +21,11 @@ Template.bookPreview.events({
     event.preventDefault();
 
     // Get ID of current book
-    const bookId = templateInstance.data.currentBook._id;
+    const bookId = templateInstance.data.book._id;
 
     // Get value from FORM
     const values = new FormData(event.currentTarget);
+
     // Create Date format using selected day, month and year
     const selectedDate = new Date(values.get('set-year'), values.get('set-month'), values.get('set-day'));
 
@@ -66,7 +36,10 @@ Template.bookPreview.events({
       date: selectedDate
     };
 
-    // Insert in collection
+    // Update read information in current document
+    templateInstance.data.book.read_information.push(insertData);
+
+    // Update read information in collection
     Books.update(bookId, { $push: { read_information: insertData }});
   },
 });
