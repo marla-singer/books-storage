fd513af1d6e780bca7de7176dd425ad650ec18ab
Daria Voytova <vdariaclem@gmail.com>
2017-02-23 10:41:09 +0300

    Create template + replace handlers for add. info block

---
+++ book-additional-info/additional-info.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+
+Template.bookAdditionalInformation.helpers({
+  setPurchaseData () {
+    const checkValue = Template.instance().setPurchaseData;
+
+    return !!(checkValue && checkValue.get());
+  }
+});
+
+Template.bookAdditionalInformation.events({
+  'change [name=set-putchase-data]' (event, templateInstance) {
+    const checkedValue = event.currentTarget.checked;
+
+    // If reactive var doesn't exist then create it
+    if (templateInstance.setPurchaseData === undefined) {
+      templateInstance.setPurchaseData = new ReactiveVar(false);
+    }
+    // Set value
+    templateInstance.setPurchaseData.set(checkedValue);
+  },
+  'click button[name="book-tag"]' (event, templateInstance) {
+    event.currentTarget.classList.toggle('active');
+  }
+});
